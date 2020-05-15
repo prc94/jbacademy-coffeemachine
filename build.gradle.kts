301bcaedf80469ba7054cf903f4380ef51d4dc09
prc94 <pro-c_x64@tutamail.com>
2020-05-15 15:08:12 +0300

    Added manifest generation at build time, made preparations for use with GitHub Actions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
+import java.util.Date
+import java.text.SimpleDateFormat
+
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.71"
+    kotlin("jvm") version "1.3.72"
+    id("maven-publish")
     application
 }
+
+project.properties["groupId"]?.let { group = it }
+project.properties["version"]?.let { version = it }
 
 repositories {
     jcenter()
@@ -13,4 +20,37 @@ dependencies {
 
 application {
     mainClassName = "machine.MainKt"
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+                "Implementation-Title" to project.name,
+                "Implementation-Vendor" to project.group,
+                "Implementation-Version" to project.version,
+                "Implementation-Timestamp" to SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz").format(Date())
+        )
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("dist") {
+            groupId = "$group"
+            artifactId = project.name
+            version = "${project.version}"
+            artifact("$buildDir/distributions/${project.name}.tar")
+            artifact("$buildDir/distributions/${project.name}.zip")
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/prc94/${project.name}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
